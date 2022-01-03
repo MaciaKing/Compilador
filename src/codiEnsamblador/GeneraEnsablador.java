57b0e89c0b004a6a3bc03aa1a34cb36853acf152
MaciaKing <51322831+MaciaKing@users.noreply.github.com>
2022-01-03 11:02:47 +0100

    .
    .

--- src/codiEnsamblador/GeneraEnsablador.java
+++ src/codiEnsamblador/GeneraEnsablador.java
@@ -6,6 +6,7 @@ package codiEnsamblador;
 
 import compilador.sintactic.Codi3A;
 import compilador.sintactic.EscritorFichero;
+import compilador.sintactic.InstrCodi3A;
 import compilador.sintactic.TablaVariables;
 
 /**
@@ -13,28 +14,62 @@ import compilador.sintactic.TablaVariables;
  * @author macia
  */
 public class GeneraEnsablador {
-   
-   //Codi3A c;
+
+    //Codi3A c;
     EscritorFichero f;
-  public GeneraEnsablador(){
-     f= new EscritorFichero("XXXXXXXX.X68");
-  }
-  
-  public void generaCodi68k(){
-     f.escrivirFichero("\tORG    $600");
-     //primero generamos las variables
-     for(int i=0; i< TablaVariables.tVar.size();i++){
-         generaVariableGlobal68k(TablaVariables.tVar.get(i).idVariable);
-     }
-     f.escrivirFichero("\n\tORG    $1000");
-     f.escrivirFichero("\nSTART:");
-     
-     f.cierraFichero();
-  }
-  
-  public void generaVariableGlobal68k(String nombre){
-      nombre+="\tDS.W 1";
-      f.escrivirFichero(nombre);
-      //f.cierraFichero();
-  }
+
+    public GeneraEnsablador() {
+        f = new EscritorFichero("XXXXXXXX.X68");
+    }
+
+    public void generaCodi68k() {
+        f.escrivirFichero("\tORG    $600");
+        //primero generamos las variables
+        for (int i = 0; i < TablaVariables.tVar.size(); i++) {
+            generaVariableGlobal68k(TablaVariables.tVar.get(i).idVariable);
+        }
+        f.escrivirFichero("\n\tORG    $1000");
+        f.escrivirFichero("\nSTART:");
+        //Ahora cogemos las instrucciones del codigo 3A
+        for (int i = 0; i < Codi3A.C3A.size(); i++) {
+             generaInstruccion68k(Codi3A.C3A.get(i));
+        }
+
+        f.escrivirFichero("\tEND START");
+        f.cierraFichero();
+    }
+
+    public void generaVariableGlobal68k(String nombre) {
+        nombre += "\tDS.W 1";
+        f.escrivirFichero(nombre);
+    }
+
+    public void generaInstruccion68k(InstrCodi3A inst) {
+        switch (inst.instruccion) {
+            case SKIP:
+                  f.escrivirFichero(inst.destino.toString() +":");
+                  break;
+            case COPY:
+                 f.escrivirFichero("\tMOVE.W #"+inst.param2 +","+inst.destino);
+                 break;
+   /*ADD,
+    SUB,
+    MUL,
+    DIV,
+    MOD,
+    AND,
+    OR,
+    LT,
+    LE,
+    EQ,
+    NE,
+    GE,
+    GT,
+    IFGT,
+    ASSIG,
+    GOTO,
+    
+    */
+        }
+    }
 }
--- src/compilador/sintactic/InstrCodi3A.java
+++ src/compilador/sintactic/InstrCodi3A.java
@@ -10,10 +10,10 @@ package compilador.sintactic;
  * @author jfher
  */
 public class InstrCodi3A {
-    TiposInstruccionC3A instruccion;
-    OperandoC3A param1;
-    OperandoC3A param2;
-    OperandoC3A destino;
+   public TiposInstruccionC3A instruccion;
+    public OperandoC3A param1;
+    public OperandoC3A param2;
+    public OperandoC3A destino;
 
     public InstrCodi3A(TiposInstruccionC3A instruccion, OperandoC3A param1, OperandoC3A param2, OperandoC3A destino) {
         this.instruccion = instruccion;
@@ -24,34 +24,36 @@ public class InstrCodi3A {
 
     @Override
     public String toString() {
-//        String devolver = instruccion + " ";
-//        
-//        if(param1 != null){
-//            devolver += param1 + " ";
-//        }else{
-//            devolver += "OP{_____} ";
-//        }
-//        if(param2 != null){
-//            devolver += param2 + " ";
-//        }else{
-//            devolver += "OP{_____} ";
-//        }
-//        if(destino != null){
-//            devolver += destino + " ";
-//        }else{
-//            devolver += "OP{_____} ";
-//        }
         String devolver = instruccion + " ";
         
         if(param1 != null){
             devolver += param1 + " ";
+        }else{
+            devolver += "OP{_____} ";
         }
         if(param2 != null){
             devolver += param2 + " ";
+        }else{
+            devolver += "OP{_____} ";
         }
         if(destino != null){
             devolver += destino + " ";
+        }else{
+            devolver += "OP{_____} ";
         }
+        
+        
+//        String devolver = instruccion + " ";
+//        
+//        if(param1 != null){
+//            devolver += param1 + " ";
+//        }
+//        if(param2 != null){
+//            devolver += param2 + " ";
+//        }
+//        if(destino != null){
+//            devolver += destino + " ";
+//        }
         return devolver;
     }
     
