dd08a018d0b9ca38ec410fce0f2803c563c750f1
MaciaKing <51322831+MaciaKing@users.noreply.github.com>
2022-01-12 17:11:36 +0100

    <=

--- src/codiEnsamblador/GeneraEnsablador.java
+++ src/codiEnsamblador/GeneraEnsablador.java
@@ -277,7 +277,7 @@ public class GeneraEnsablador {
                 break;
                 
             case GE:
-                 f.escribirFichero("\t ;GE " + inst.param1 + "," + inst.param2 + "," + inst.destino);
+                f.escribirFichero("\t ;GE " + inst.param1 + "," + inst.param2 + "," + inst.destino);
                 if (inst.param1.type.equals(TiposOperandoC3A.enteroLit)) { // (# > ?)
                     if (inst.param2.type.equals(TiposOperandoC3A.enteroLit)) { // (# > #)
                         f.escribirFichero("\tMOVE.W #" + inst.param1 + ", D0");
@@ -304,6 +304,37 @@ public class GeneraEnsablador {
                         f.escribirFichero("\tBGE " + inst.destino);
                     }
                 }
+                break;
+                
+            case LE:
+                f.escribirFichero("\t ;LE " + inst.param1 + "," + inst.param2 + "," + inst.destino);
+                if (inst.param1.type.equals(TiposOperandoC3A.enteroLit)) { // (# > ?)
+                    if (inst.param2.type.equals(TiposOperandoC3A.enteroLit)) { // (# > #)
+                        f.escribirFichero("\tMOVE.W #" + inst.param1 + ", D0");
+                        f.escribirFichero("\tMOVE.W #" + inst.param2 + ", D1");
+                        f.escribirFichero("\tCMP.W  D1, D0");
+                        f.escribirFichero("\tBLE " + inst.destino);
+                    }else{ // (# > v)
+                        f.escribirFichero("\tMOVE.W #" + inst.param1 + ", D0");
+                        f.escribirFichero("\tMOVE.W (" + inst.param2 + "), D1");
+                        f.escribirFichero("\tCMP.W  D1, D0");
+                        f.escribirFichero("\tBLE " + inst.destino);                        
+                    }
+
+                } else { // (v > ?)
+                    if (inst.param2.type.equals(TiposOperandoC3A.enteroLit)) { // (v > #)
+                        f.escribirFichero("\tMOVE.W (" + inst.param1 + "), D0");
+                        f.escribirFichero("\tMOVE.W #" + inst.param2 + ", D1");
+                        f.escribirFichero("\tCMP.W  D1, D0");
+                        f.escribirFichero("\tBLE " + inst.destino);
+                    } else { // (v > v)
+                        f.escribirFichero("\tMOVE.W (" + inst.param1 + "), D0");
+                        f.escribirFichero("\tMOVE.W (" + inst.param2 + "), D1");
+                        f.escribirFichero("\tCMP.W  D1, D0");
+                        f.escribirFichero("\tBLE " + inst.destino);
+                    }
+                } 
+                
                 break;
 
             case EQ:
