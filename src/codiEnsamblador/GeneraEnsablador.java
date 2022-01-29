e773eb57c4102d0fa1bc5d2db2298388cb030408
MaciaKing <51322831+MaciaKing@users.noreply.github.com>
2022-01-29 10:37:53 +0100

    .
    .

--- src/codiEnsamblador/GeneraEnsablador.java
+++ src/codiEnsamblador/GeneraEnsablador.java
@@ -305,27 +305,27 @@ public class GeneraEnsablador {
                 f.escribirFichero("\t ;GE " + inst.param1 + "," + inst.param2 + "," + inst.destino);
                 if (inst.param1.type.equals(TiposOperandoC3A.enteroLit)) { // (# > ?)
                     if (inst.param2.type.equals(TiposOperandoC3A.enteroLit)) { // (# > #)
-                        f.escribirFichero("\tMOVE.W #" + inst.param1 + ",D0");
-                        f.escribirFichero("\tMOVE.W #" + inst.param2 + ",D1");
-                        f.escribirFichero("\tCMP.W  D1, D0");
+                        f.escribirFichero("\tMOVE.L #" + inst.param1 + ",D0");
+                        f.escribirFichero("\tMOVE.L #" + inst.param2 + ",D1");
+                        f.escribirFichero("\tCMP.L  D1, D0");
                         f.escribirFichero("\tBGE " + inst.destino);
                     } else { // (# > v)
-                        f.escribirFichero("\tMOVE.W #" + inst.param1 + ",D0");
-                        f.escribirFichero("\tMOVE.W (" + inst.param2 + "),D1");
-                        f.escribirFichero("\tCMP.W  D1,D0");
+                        f.escribirFichero("\tMOVE.L #" + inst.param1 + ",D0");
+                        f.escribirFichero("\tMOVE.L (" + inst.param2 + "),D1");
+                        f.escribirFichero("\tCMP.L  D1,D0");
                         f.escribirFichero("\tBGE " + inst.destino);
                     }
 
                 } else { // (v > ?)
                     if (inst.param2.type.equals(TiposOperandoC3A.enteroLit)) { // (v > #)
-                        f.escribirFichero("\tMOVE.W (" + inst.param1 + "),D0");
-                        f.escribirFichero("\tMOVE.W #" + inst.param2 + ",D1");
-                        f.escribirFichero("\tCMP.W  D1, D0");
+                        f.escribirFichero("\tMOVE.L (" + inst.param1 + "),D0");
+                        f.escribirFichero("\tMOVE.L #" + inst.param2 + ",D1");
+                        f.escribirFichero("\tCMP.L  D1, D0");
                         f.escribirFichero("\tBGE " + inst.destino);
                     } else { // (v > v)
-                        f.escribirFichero("\tMOVE.W (" + inst.param1 + "),D0");
-                        f.escribirFichero("\tMOVE.W (" + inst.param2 + "),D1");
-                        f.escribirFichero("\tCMP.W  D1,D0");
+                        f.escribirFichero("\tMOVE.L (" + inst.param1 + "),D0");
+                        f.escribirFichero("\tMOVE.L (" + inst.param2 + "),D1");
+                        f.escribirFichero("\tCMP.L  D1,D0");
                         f.escribirFichero("\tBGE " + inst.destino);
                     }
                 }
@@ -335,27 +335,27 @@ public class GeneraEnsablador {
                 f.escribirFichero("\t ;LE " + inst.param1 + "," + inst.param2 + "," + inst.destino);
                 if (inst.param1.type.equals(TiposOperandoC3A.enteroLit)) { // (# > ?)
                     if (inst.param2.type.equals(TiposOperandoC3A.enteroLit)) { // (# > #)
-                        f.escribirFichero("\tMOVE.W #" + inst.param1 + ",D0");
-                        f.escribirFichero("\tMOVE.W #" + inst.param2 + ",D1");
-                        f.escribirFichero("\tCMP.W  D1,D0");
+                        f.escribirFichero("\tMOVE.L #" + inst.param1 + ",D0");
+                        f.escribirFichero("\tMOVE.L #" + inst.param2 + ",D1");
+                        f.escribirFichero("\tCMP.L  D1,D0");
                         f.escribirFichero("\tBLE " + inst.destino);
                     } else { // (# > v)
-                        f.escribirFichero("\tMOVE.W #" + inst.param1 + ",D0");
-                        f.escribirFichero("\tMOVE.W (" + inst.param2 + "),D1");
-                        f.escribirFichero("\tCMP.W  D1,D0");
+                        f.escribirFichero("\tMOVE.L #" + inst.param1 + ",D0");
+                        f.escribirFichero("\tMOVE.L (" + inst.param2 + "),D1");
+                        f.escribirFichero("\tCMP.L  D1,D0");
                         f.escribirFichero("\tBLE " + inst.destino);
                     }
 
                 } else { // (v > ?)
                     if (inst.param2.type.equals(TiposOperandoC3A.enteroLit)) { // (v > #)
-                        f.escribirFichero("\tMOVE.W (" + inst.param1 + "),D0");
-                        f.escribirFichero("\tMOVE.W #" + inst.param2 + ",D1");
-                        f.escribirFichero("\tCMP.W  D1, D0");
+                        f.escribirFichero("\tMOVE.L (" + inst.param1 + "),D0");
+                        f.escribirFichero("\tMOVE.L #" + inst.param2 + ",D1");
+                        f.escribirFichero("\tCMP.L  D1, D0");
                         f.escribirFichero("\tBLE " + inst.destino);
                     } else { // (v > v)
-                        f.escribirFichero("\tMOVE.W (" + inst.param1 + "),D0");
-                        f.escribirFichero("\tMOVE.W (" + inst.param2 + "),D1");
-                        f.escribirFichero("\tCMP.W  D1,D0");
+                        f.escribirFichero("\tMOVE.L (" + inst.param1 + "),D0");
+                        f.escribirFichero("\tMOVE.L (" + inst.param2 + "),D1");
+                        f.escribirFichero("\tCMP.L  D1,D0");
                         f.escribirFichero("\tBLE " + inst.destino);
                     }
                 }
@@ -365,22 +365,22 @@ public class GeneraEnsablador {
                 f.escribirFichero("\t ;LT " + inst.param1 + "," + inst.param2 + "," + inst.destino);
                 if (inst.param1.type.equals(TiposOperandoC3A.enteroLit)) { // (# > ?)
                     if (inst.param2.type.equals(TiposOperandoC3A.enteroLit)) { // (# > #)
-                        f.escribirFichero("\tMOVE.W #" + inst.param1 + ",D0");
-                        f.escribirFichero("\tMOVE.W #" + inst.param2 + ",D1");
-                        f.escribirFichero("\tCMP.W  D1,D0");
+                        f.escribirFichero("\tMOVE.L #" + inst.param1 + ",D0");
+                        f.escribirFichero("\tMOVE.L #" + inst.param2 + ",D1");
+                        f.escribirFichero("\tCMP.L  D1,D0");
                         f.escribirFichero("\tBLT " + inst.destino);
                     } else { // (# > v)
-                        f.escribirFichero("\tMOVE.W #" + inst.param1 + ",D0");
-                        f.escribirFichero("\tMOVE.W (" + inst.param2 + "),D1");
-                        f.escribirFichero("\tCMP.W  D1,D0");
+                        f.escribirFichero("\tMOVE.L #" + inst.param1 + ",D0");
+                        f.escribirFichero("\tMOVE.L (" + inst.param2 + "),D1");
+                        f.escribirFichero("\tCMP.L  D1,D0");
                         f.escribirFichero("\tBLT " + inst.destino);
                     }
 
                 } else { // (v > ?)
                     if (inst.param2.type.equals(TiposOperandoC3A.enteroLit)) { // (v > #)
-                        f.escribirFichero("\tMOVE.W (" + inst.param1 + "),D0");
-                        f.escribirFichero("\tMOVE.W #" + inst.param2 + ",D1");
-                        f.escribirFichero("\tCMP.W  D1,D0");
+                        f.escribirFichero("\tMOVE.L (" + inst.param1 + "),D0");
+                        f.escribirFichero("\tMOVE.L #" + inst.param2 + ",D1");
+                        f.escribirFichero("\tCMP.L  D1,D0");
                         f.escribirFichero("\tBLT " + inst.destino);
                     } else { // (v > v)
                         f.escribirFichero("\tMOVE.W (" + inst.param1 + "),D0");
@@ -392,6 +392,7 @@ public class GeneraEnsablador {
                 break;
 
             case EQ:
+                //f.escribirFichero("------");
                 if (inst.temporal) {
                     f.escribirFichero("\t ;EQUAAAAAL----TEMPORAL " + TablaVariables.tVar.get(Integer.parseInt(inst.param1.operando)).idVariable + ", " + inst.param2.operando + "," + inst.destino);
                     f.escribirFichero("\tMOVE.L (" + TablaVariables.tVar.get(Integer.parseInt(inst.param1.operando)).idVariable + "), D0");
